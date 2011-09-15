7347d4865f6881d8f2cc31de86cc313344cecfc6
Keiouu <keiouu@gmail.com>
2011-09-15 10:18:34 +0100

    Added media files, initial admin pages

---
+++ apps/admin/templates/includes/header.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Evelite Admin App Header
+ *
+ * Copyright 2011, AUTHORS.txt
+ * Licensed under the GNU General Public License version 3.
+ * See LICENSE.txt
+ */
+?>
+
+<!doctype html> 
+<html lang="en">
+<head> 
+  <meta charset="utf-8">
+ 
+  <title><?php print $title; ?> | Evelite Admin</title> 
+  <meta name="description" content="Evelite"> 
+  <meta name="author" content="Evelite"> 
+</head> 
+ 
+<body>
+  
--- apps/admin/templates/index.php
+++ apps/admin/templates/index.php
@@ -1,25 +1,24 @@
 <?php
 /*
- * Evelite Core App Index Page
+ * Evelite Admin App Index Page
  *
  * Copyright 2011, AUTHORS.txt
  * Licensed under the GNU General Public License version 3.
  * See LICENSE.txt
  */
 
-$title = "Home Page | Evelite";
-include("header.php");
+$title = "Home";
+include("includes/header.php");
 ?>
 
-<h1>Welcome to Evelite!</h1>
+<h1>Welcome to Evelite Admin!</h1>
 <ul>
-<li><a href="/admin/">Admin Portal</a></li>
-<li><a href="/test/">Run Tests</a></li>
+<li><a href="/">Home</a></li>
 </ul>
 
 <?php
 if ($request->get_page_load_time() !== False)
 	print "<p>Page loaded in: " . $request->get_page_load_time() . " seconds</p>";
-include("footer.php");
+include("includes/footer.php");
 ?>
 

2e69eefbc8e52ea798897437a5687552bd7a6ca6
Keiouu <keiouu@gmail.com>
2011-09-15 12:30:36 +0100

    Few minor style changes

--- apps/admin/templates/includes/header.php
+++ apps/admin/templates/includes/header.php
@@ -6,6 +6,7 @@
  * Licensed under the GNU General Public License version 3.
  * See LICENSE.txt
  */
+$media_url = $GLOBALS["media_url"];
 ?>
 
 <!doctype html> 
@@ -17,14 +18,14 @@
   <meta name="description" content="Evelite"> 
   <meta name="author" content="Evelite">
   
-  <link rel="stylesheet" type="text/css" href="/media/themes/default/theme.css" media="screen" /> 
-  <link rel="stylesheet" type="text/css" href="/apps/admin/media/themes/default/style.css" media="screen" />
+  <link rel="stylesheet" type="text/css" href="<?php print $media_url; ?>media/themes/default/theme.css" media="screen" /> 
+  <link rel="stylesheet" type="text/css" href="<?php print $media_url; ?>apps/admin/media/themes/default/style.css" media="screen" />
   
-  <script src="/media/jQuery/jquery-1.6.4.min.js" type="text/javascript"></script>
-  <script src="/media/jQuery/ui/jquery-ui-1.8.16.min.js" type="text/javascript"></script>
+  <script src="<?php print $media_url; ?>media/jQuery/jquery-1.6.4.min.js" type="text/javascript"></script>
+  <script src="<?php print $media_url; ?>media/jQuery/ui/jquery-ui-1.8.16.min.js" type="text/javascript"></script>
 
 </head> 
  
 <body>
-<div id="navbar"></div>
+<div id="userbar" class="ui-widget ui-widget-header"></div>
   
